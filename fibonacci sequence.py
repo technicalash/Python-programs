398020c0b30595529257df06c8a5cb525db21376
technicalash <atoz8773h@gmail.com>
2024-10-12 22:10:59 +0530

    Add files via upload

---
+++ fibonacci sequence.py
@@ -0,0 +1,15 @@
+#fibonacci sequence
+n=int(input("enter the whole number upto which you want to generate fibonacci sequence"))
+a=0
+b=1
+print(a)
+print(b)
+for i in range(0,n):
+    print(a+b)
+    a,b=b,a+b
+    
+
+    
+    
+    
+
